767241e473f7310f41513a5ea4e5523886cb3b94
Ilikedogs <33440358+Ilikedogs1234@users.noreply.github.com>
2019-12-05 20:43:36 -0500

    Pre-sub changes

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-$(document).ready(function(){
+var destination = ""
+var departure = ""
+var startTime = ""
+var endTime = ""
+var date = ""
+var price = 0
 
+
+$(document).ready(function(){
+    $('#page2').hide();
     $('#flightCard').on('click', function() {  
-        alert(1);
+        destination="Charleston"
+        departure="Atlanta"
+        date = "5/22/20"
+        price = 150
+        startTime="10:50"
+        endTime = "3:20"
+        $('#page1').hide();
+        $('#page2').show();
+        $('#travelInfo').text(departure + " -> " + destination);
+        $('#startTime').text("Start Time: " + startTime)
+        $('#endTime').text("End Time: " + endTime)
+        $('#date').text(date)
+        $('#price').text("$" + price)
     });
 });
